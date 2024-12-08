7a5ed06864b135e8c91d1642c97265cb1c29780b
gioarango <gioarango.94@gmail.com>
2024-12-08 01:22:10 -0500

    express middleware is added to parser the request

--- src/server.js
+++ src/server.js
@@ -3,22 +3,23 @@ import cors from 'cors';
 
 class Server {
     constructor(options) {
-        const { port, routes } = options;
-        this.app = express();
-        this.port = port;
-        this.routes = routes;        
+        const { port, routes } = options
+        this.app = express()
+        this.port = port
+        this.routes = routes
     }
 
     async start() {
 
         // middlewares
-        this.app.use(cors());
+        this.app.use(cors())
+        this.app.use(express.json())
 
         // routes
-        this.app.use(this.routes);
+        this.app.use(this.routes)
 
         this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`);
+            console.log(`Server running on port ${this.port}`)
         });
     }
 }
