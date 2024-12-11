d8328c6fee734f1c91fcde05606d5c56933f01ec
gioarango <gioarango.94@gmail.com>
2024-12-10 22:08:00 -0500

    lambda handler is created

---
+++ src/lambda/handler.js
@@ -0,0 +1,23 @@
+import { MutantController } from '../controllers/mutants/controller.js';
+import { MutantServices } from '../services/mutants/services.js';
+
+export const handler = async (event) => {
+    try {
+        console.log("EVENT", event, typeof event)
+
+        const mutantService = new MutantServices();
+        const mutantController = new MutantController(mutantService);
+
+        const { dna } = event
+
+        const result = await mutantController.searchMutant(dna);
+
+        return {
+            statusCode: result ? 200 : 403
+        }
+    } catch (error) {
+        return {
+            statusCode: error.statusCode || 500
+        }
+    }
+};
