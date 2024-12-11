883a3d0a92ba6057ab59755d8097b1cb8bbcf97f
gioarango <gioarango.94@gmail.com>
2024-12-10 22:08:38 -0500

    the controller is adapted to integrate with lambda flow

--- src/controllers/mutants/controller.js
+++ src/controllers/mutants/controller.js
@@ -1,35 +1,27 @@
 import { config } from "../../config/index.js"
 import { buildMatrix } from "../../utils/buildMatrix.js"
+import { validateDNA } from "../../utils/validateDNA.js"
 
 class MutantController {
-    
+
     constructor(mutantServices) {
         this.mutantServices = mutantServices
     }
 
-    searchMutant = (req, res) => {
-        try {
-            const { dna } = req.body
-            
-            const matrix = buildMatrix(dna)
-            const sequenceLength = config.SEQUENCE
-    
-            const totalSequence =
-            this.mutantServices.searchHorizontal(matrix, sequenceLength) + 
-            this.mutantServices.searchVertical(matrix, sequenceLength) + 
+    async searchMutant(dna) {
+        validateDNA(dna)
+
+        const matrix = buildMatrix(dna)
+
+        const sequenceLength = config.SEQUENCE
+
+        const totalSequence =
+            this.mutantServices.searchHorizontal(matrix, sequenceLength) +
+            this.mutantServices.searchVertical(matrix, sequenceLength) +
             this.mutantServices.searchMainDiagonals(matrix, sequenceLength) +
             this.mutantServices.searchInvertedDiagonals(matrix, sequenceLength)
-    
-            const isMutant = totalSequence > 1;
-    
-            if(!isMutant) {
-                res.status(403).send()
-            }
-            
-            res.status(200).send()            
-        } catch (error) {
-            res.status(500).json({ msg: `Internal server error: ${error.message}` })
-        }
+
+        return totalSequence > 1;
     }
 }
 
